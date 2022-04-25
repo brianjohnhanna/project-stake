0bfaf5b5dfe1ad2c6b321164c989b01410c240b7
Brian Hanna <bjhanna728@gmail.com>
2022-04-24 23:12:58 -0400

    General bugfixes

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
-import './style.css'
-import './modernizr'
-import { gsap } from "gsap";
+import './style.css';
+import './modernizr';
+import { gsap } from 'gsap';
 import imagesloaded from 'imagesloaded';
 
 function doors() {
-  gsap.to("#doorLeft", { x: '-100%', duration: 30, ease: "elastic" });
-  gsap.to("#doorRight", { x: '100%', duration: 30, ease: "elastic" });
+  gsap.to('#doorLeft', { x: '-100%', duration: 30, ease: 'elastic' });
+  gsap.to('#doorRight', { x: '100%', duration: 30, ease: 'elastic' });
+  gsap.to('#bridge', { opacity: 1, delay: 0.1 });
 }
 
 function captains() {
   const offsetX = 50;
   const offsetY = 25;
-  gsap.fromTo('#captainLeft', { x: offsetX, y: -offsetY }, { x: 0, y: 0, duration: 6, delay: 0.5, ease: "expo" });
-  gsap.fromTo('#captainRight', { x: -offsetX, y: -offsetY }, { x: 0, y: 0, duration: 6, delay: 0.5, ease: "expo"});
+  gsap.fromTo(
+    '#captainLeft',
+    { x: offsetX, y: -offsetY },
+    { x: 0, y: 0, duration: 6, delay: 0.5, ease: 'expo' }
+  );
+  gsap.fromTo(
+    '#captainRight',
+    { x: -offsetX, y: -offsetY },
+    { x: 0, y: 0, duration: 6, delay: 0.5, ease: 'expo' }
+  );
 }
 
 function runAnimation() {
@@ -21,7 +30,6 @@ function runAnimation() {
 }
 
 imagesloaded('#animationWrapper', () => {
-  setTimeout(runAnimation, 1000);
   document.getElementById('animationWrapper').classList.remove('hidden');
-  document.getElementById('bridge').classList.remove('hidden');
-} );
+  setTimeout(runAnimation, 1000);
+});
